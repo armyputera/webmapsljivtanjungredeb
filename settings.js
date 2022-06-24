34e4cd62f772194d3b8edfd0461b7388ac228763
armyputera <armyputera@gmail.com>
2022-06-24 18:08:23 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,8 +7,8 @@ const mapCenter = [2.1171, 116.8123];
 const mapZoom = 14;
 
 // Marker icon height and width
-const iconHeight = 10;
-const iconWidth = 10;
+const iconHeight = 25;
+const iconWidth = 25;
 
 //Icon for pohon muda and pohon dewasa
 const sudahTebang =  'logo/pohon_dewasa.svg'
